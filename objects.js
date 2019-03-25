76a3d29f03bf94e3c495ad8c414a1f904f35d2dd
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-25 23:02:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {
   cher: "turn back time",
   rupaul: "sissy that walk"
 };
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[prince]: 'purple rain'})
+}
