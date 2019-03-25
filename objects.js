b907343b87964592fc8a30315cb8a6f39d84336c
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-25 23:02:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ var playlist = {
 };
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[prince]: 'purple rain'})
+  return Object.assign({}, playlist, {[artistName]: 'songTitle'})
 }
